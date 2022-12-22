7e5e6f872f9e975f211316b4c76d4ba72f16772c
Fernando Mendes <fernando.abreu@outlook.com>
2022-12-22 14:14:19 -0300

    Exemplo com o keycloak

---
+++ Marraia.KeyCloak/Marraia.KeyCloak.API/Program.cs
@@ -0,0 +1,51 @@
+using Keycloak.AuthServices.Authentication;
+using Keycloak.AuthServices.Authorization;
+using Keycloak.AuthServices.Sdk.Admin;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var authenticationOptions = builder
+                            .Configuration
+                            .GetSection(KeycloakAuthenticationOptions.Section)
+                            .Get<KeycloakAuthenticationOptions>();
+
+builder.Services.AddKeycloakAuthentication(authenticationOptions);
+
+var authorizationOptions = builder
+                            .Configuration
+                            .GetSection(KeycloakProtectionClientOptions.Section)
+                            .Get<KeycloakProtectionClientOptions>();
+
+builder.Services.AddKeycloakAuthorization(authorizationOptions);
+
+var adminClientOptions = builder
+                            .Configuration
+                            .GetSection(KeycloakAdminClientOptions.Section)
+                            .Get<KeycloakAdminClientOptions>();
+
+builder.Services.AddKeycloakAdminHttpClient(adminClientOptions);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
