9d8f5656309e35a7d41f098dba29b476a60c2eb4
Fernando Mendes <fernando.abreu@outlook.com>
2023-12-11 15:18:04 -0300

    [Update] - Criação de duas propriedades na classe Weather

--- Marraia.KeyCloak/Marraia.KeyCloak.API/WeatherForecast.cs
+++ Marraia.KeyCloak/Marraia.KeyCloak.API/WeatherForecast.cs
@@ -9,5 +9,8 @@ namespace Marraia.KeyCloak.API
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+
+        public int MyProperty { get; set; }
+        public int MyProperty1 { get; set; }
     }
 }
